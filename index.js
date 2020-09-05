2d466934f455f447e4ddce1a09db25f080f846cc
Todd Stoneberg <todd.stoneberg@gmail.com>
2020-09-05 02:38:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 function happyHolidays() {
   return 'Happy holidays!'
 }
+
+function happyHolidaysToName(name) {
+  return `Happy holidays, ${name}!`
+}
+
+function happyCustomHolidayTo(holiday, name) {
+  return `Happy ${holidy}, ${name}`
+}
